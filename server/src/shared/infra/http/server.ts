932ce0e677e37636d82e30a2d73355c94de36ca4
Henrique Ramos <henriqueramos0106@gmail.com>
2020-10-17 10:44:35 -0300

    Configuring editorconfig, eslint and prettier at server

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 import '../typeorm';
 
 const app = express();
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.get('/users', (request, response) => {
   return response.json({ message: 'Hello World' });
